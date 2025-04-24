2180ebf6df5bf8bf0467e80e0b68e5bb1963003a
Satyam Gupta <satyamg@valuefy.com>
2025-04-25 00:01:41 +0530

    Spring security init

--- src/main/java/com/journalIt/journalIt/JournalItApplication.java
+++ src/main/java/com/journalIt/journalIt/JournalItApplication.java
@@ -30,3 +30,4 @@ public class JournalItApplication {
 // 		 1- On higher level Session = Transactional context;
 //  	 2- dbFactory helps us to create connection with mongoDB
 // 		 3- Without the implementation of this [PlatformTransactionManager] who's going to handle the transaction
+// 		 4- BEAN ?? "Hey Spring, when you build the application context, call this method and manage the returned object (MongoTransactionManager) as a bean."
